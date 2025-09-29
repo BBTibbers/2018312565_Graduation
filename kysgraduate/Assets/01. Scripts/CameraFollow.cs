82bbbe010fd245a4666447e5413bc2844d998ac8
BBTibbers <youngsuk0311@gmail.com>
2025-09-30 05:45:30 +0900

    9/30

---
+++ kysgraduate/Assets/01. Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private PlayerMove _player;
+    private Vector3 _topPosition = new Vector3(0f, 5f, -3f);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player == null)
+            _player = FindFirstObjectByType<PlayerMove>();
+
+        transform.position = _player.transform.position + _topPosition;
+    }
+}
